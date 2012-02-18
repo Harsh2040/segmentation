262f17a25c19078c46a6dd966f090e5a08cce75d
Joshua Antonishen <joshua@antonishen.com>
2012-02-18 13:36:30 -0500

    refactor spec

--- spec/binary_image_spec.rb
+++ spec/binary_image_spec.rb
@@ -2,13 +2,17 @@ require_relative 'spec_helper'
 
 describe BinaryImage do
 
-  raw_image = [[1,0,0,0,0,1],
-                [1,1,0,1,0,1],
-                [1,1,0,0,1,1]]
+  raw_image = [[1,0,0,1,0,1],
+               [1,1,0,0,0,1],
+               [1,1,1,1,1,1]]
 
   let(:image) { BinaryImage.new(raw_image) }
-
   subject{ image }
-  its(:rows) { should == 3 }
-  its(:columns) { should == 6 }
+
+  describe "#initialize" do
+    describe "it should correctly set the rows and columns" do
+      its(:rows) { should == 3 }
+      its(:columns) { should == 6 }
+    end
+  end
 end
