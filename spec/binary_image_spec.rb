bec5e2bbbd5095a07e5afa67ddfdf0fa1d2b78b5
Joshua Antonishen <joshua@antonishen.com>
2012-02-18 17:13:56 -0500

    fixed bug where tests were order dependent

--- spec/binary_image_spec.rb
+++ spec/binary_image_spec.rb
@@ -2,14 +2,15 @@ require_relative 'spec_helper'
 
 describe BinaryImage do
 
-  raw_image = [[1,0,0,1,0,1],
-               [1,1,0,0,0,1],
-               [1,1,1,1,1,1]]
+  let(:raw_image) { [[1,0,0,1,0,1],
+                     [1,1,0,0,0,1],
+                     [1,1,1,1,1,1]] }
 
   let(:image) { BinaryImage.new(raw_image) }
-  subject{ image }
 
   describe "#initialize" do
+    subject{ image }
+
     describe "it should correctly set the rows and columns" do
       its(:rows) { should == 3 }
       its(:columns) { should == 6 }
