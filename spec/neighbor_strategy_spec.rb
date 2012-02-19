4386873e9a2b112541a2a3e9f995a0eb2dc30bb1
Joshua Antonishen <joshua@antonishen.com>
2012-02-18 20:14:14 -0500

    test default behavior

--- spec/neighbor_strategy_spec.rb
+++ spec/neighbor_strategy_spec.rb
@@ -5,6 +5,16 @@ describe NeighborStrategy do
     let(:processed) { strategy.processed }
     let(:not_processed) { strategy.not_processed }
 
+    context "default neighbor" do
+      let(:strategy) { strategy = NeighborStrategy.new() }
+
+      subject { processed }
+      its(:length) { should == 2 }
+
+      subject { not_processed }
+      its(:length) { should == 2 }
+    end
+
     context "four neighbor" do
       let(:strategy) { strategy = NeighborStrategy.new(4) }
 
